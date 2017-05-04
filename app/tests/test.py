0923afe4e43efc34571d41d93443801b59f700ec
Marundu <marundu@gmail.com>
2017-05-04 12:13:31 +0300

    Add initial unit-test file

---
+++ app/tests/test.py
@@ -0,0 +1,28 @@
+import unittest
+from app import app
+
+class ProjectTests(unittest.TestCase):
+    # executed before each test
+    def setUp(self):
+        app.config['TESTING']=True
+        app.config['DEBUG']=False
+        self.app=app.test_client()
+        
+        self.assertEquals(app.debug, False)
+    
+    # executed after each test
+    def tearDown(self):
+        pass
+    
+    # tests
+    def test_main_page(self):
+        response=self.app.get('/', follow_redirects=True)
+        self.assertIn(b'Welcome to the Recipe App!', response.data)
+        self.assertIn(b'This site describes my favourite recipes', response.data)
+        self.assertIn(b'Breakfast', response.data)
+        self.assertIn(b'Lunch', response.data)
+        self.assertIn(b'Supper', response.data)
+        self.assertIn(b'Dessert', response.data)
+
+if __name__=='__main__':
+    unittest.main()

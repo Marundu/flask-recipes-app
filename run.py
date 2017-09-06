6ae4b840267d4767e0d6b469990fe37c86c8755c
Marundu <marundu@gmail.com>
2017-09-06 16:48:45 +0300

    Fixed A LOT of syntax errors

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-from flask import Flask
-
-app=Flask(__name__, instance_relative_config=True)
-app.config.from_pyfile('flask.cfg')
-
-from . import views
+from app import app
 
 if __name__=='__main__':
     app.run()
